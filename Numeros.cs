5e88b5eaf3ae21c291ce762bf95043700354038c
josemeroq <JAMQ1316762374@gmail.com>
2022-05-15 22:05:09 -0500

    Agregar archivos de proyecto.

---
+++ Numeros.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoseMero3AEJ4
+{
+    public class Numeros
+    {
+        private int num { set; get; }
+
+        public Numeros(int num)
+        {
+            this.num = num;
+        }
+
+        public void numerohasta()
+        {
+           /* do
+            {
+
+                Console.WriteLine(--num);
+
+            } while (num != 0); */
+
+            if (num < 1)
+            {
+                do
+                {
+
+                    Console.WriteLine(++num);
+
+                } while (num != 0);
+            }
+            else
+            {
+                do
+                {
+
+                    Console.WriteLine(--num);
+
+                } while (num != 0);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JoseMero3AEJ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numeros numeroh = new Numeros(51);
+            numeroh.numerohasta();
+        }
+    }
+}
